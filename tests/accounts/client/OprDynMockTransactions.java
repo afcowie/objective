b4a125284785aa8d60f9d984a35e6040a181f168
Andrew Cowie <andrew@operationaldynamics.com>
2005-12-28 05:15:24 +0000

    Update MockTransactions with better posts and padZero identifier
    Add another mock Transaction.
    
    Switch to using the DataStore.getLedger() finder, doing away with much
    ugly manual fetching of ledgers (needed to feed to [] Transaction
    constructors).
    
    Add an identifier to the Transactions added by this demo program,
    padding it with zeros.

--- tests/accounts/client/OprDynMockTransactions.java
+++ tests/accounts/client/OprDynMockTransactions.java
@@ -9,11 +9,7 @@ package accounts.client;
 import generic.util.Debug;
 
 import java.io.FileNotFoundException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
-import accounts.domain.Account;
 import accounts.domain.Amount;
 import accounts.domain.Credit;
 import accounts.domain.Datestamp;
@@ -30,13 +26,7 @@ import accounts.services.PostTransactionCommand;
 /**
  * Contains a prelinary main() method and program initialization (much of which
  * will move to an eventual Client class). The remainder is a huge number of
- * Commands, instantiated programmatically, to set up *our* books. At some
- * point, probably after Balance Sheet, Income Statement, and Verification test
- * are complete, we will stop using this.
- * <P>
- * This is all programmatic - there's no GUI here.
- * <P>
- * <B>NOTE THAT RUNNING THIS DESTROYS ANY EXISTING INSTANCE OF THE DEMO_DATABASE</B>
+ * Commands, instantiated programmatically, to run a bunch of transactions.
  */
 public class OprDynMockTransactions
 {
@@ -65,67 +55,33 @@ public class OprDynMockTransactions
 		try {
 			uow = new UnitOfWork("Add Transactions to Demo Books");
 
-			Ledger gstPayable = null;
-			Ledger pettyCash = null;
-			Ledger ownersEquity = null;
-
 			/*
 			 * Fetch some accounts' ledgers
 			 */
 
-			Ledger protoL = new Ledger();
-			protoL.setName("Paid");
-
-			// TODO need query account+ledger exposure on DataStore beside
-			// getBooks
-
-			List result = ObjectiveAccounts.store.query(protoL);
-
-			if (result.size() == 1) {
-				gstPayable = (Ledger) result.get(0);
-			} else {
-				throw new Exception(result.size() + " results returned, as opposed to the 1 we wanted");
-			}
-
-			Account protoA = new Account();
-			protoA.setTitle("Petty Cash");
-
-			result = ObjectiveAccounts.store.query(protoA);
-
-			if (result.size() == 3) {
-				Account p = (Account) result.get(2);
-				Set s = p.getLedgers();
-				Iterator iter = s.iterator();
-				pettyCash = (Ledger) iter.next();
-			} else {
-				throw new Exception(result.size() + " results returned, as opposed to the 1 we wanted");
-			}
-
-			protoA = new Account();
-			protoA.setTitle("Owner's Equity");
-
-			result = ObjectiveAccounts.store.query(protoA);
-
-			if (result.size() == 1) {
-				Account p = (Account) result.get(0);
-				Set s = p.getLedgers();
-				Iterator iter = s.iterator();
-				ownersEquity = (Ledger) iter.next();
-			} else {
-				throw new Exception(result.size() + " results returned, as opposed to the 1 we wanted");
-			}
+			Ledger pettyCash = ObjectiveAccounts.store.getLedger("Petty Cash", "Manly");
+			Ledger ownersEquity = ObjectiveAccounts.store.getLedger("Owner's Equity", "");
+			Ledger groundTransport = ObjectiveAccounts.store.getLedger("Travel Expenses", "Ground Transfer");
 
 			/*
 			 * Now start storing some transactions
 			 */
 
 			Transaction[] initialization = {
-				new GenericTransaction("Initial capitalization", new Datestamp("19 Dec 03"), new Entry[] {
-						new Credit(new Amount("1.00"), ownersEquity), new Debit(new Amount("1.00"), pettyCash),
-				}),
+					new GenericTransaction("Initial capitalization", new Datestamp("19 Dec 03"), new Entry[] {
+							new Credit(new Amount("1.00"), ownersEquity), new Debit(new Amount("1.00"), pettyCash),
+					}),
+					new GenericTransaction("Loan from owner", new Datestamp("21 Dec 03"), new Entry[] {
+							new Credit(new Amount("52700.00"), ownersEquity),
+							new Debit(new Amount("52700.00"), pettyCash),
+					}),
+					new GenericTransaction("Taxi from airport", new Datestamp("5 Nov 05"), new Entry[] {
+							new Credit(new Amount("9.99"), pettyCash), new Debit(new Amount("9.99"), groundTransport),
+					}),
 			};
 
 			for (int i = 0; i < initialization.length; i++) {
+				initialization[i].setIdentifier("I" + padZeros(i, 4));
 				PostTransactionCommand cmd = new PostTransactionCommand(initialization[i]);
 				cmd.execute(uow);
 			}
@@ -142,5 +98,18 @@ public class OprDynMockTransactions
 
 		ObjectiveAccounts.store.close();
 		Debug.print("main", "Done.");
+	}
+
+	/**
+	 * Quickly prepend zeros to a number.
+	 */
+	protected static String padZeros(int num, int width) {
+		StringBuffer buf = new StringBuffer(Integer.toString(num));
+
+		for (int i = width - buf.length(); i > 0; i--) {
+			buf.insert(0, '0');
+		}
+
+		return buf.toString();
 	}
 }
