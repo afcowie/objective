275859ec51cd3cbc5ff6935e967c31faf9ab9cb5
Andrew Cowie <andrew@operationaldynamics.com>
2005-12-30 11:08:36 +0000

    Add Transactions to end of demo dump output

--- tests/accounts/ui/OprDynOutputDump.java
+++ tests/accounts/ui/OprDynOutputDump.java
@@ -7,11 +7,16 @@
 package accounts.ui;
 
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
 
 import accounts.client.OprDynBooksSetup;
 import accounts.domain.Books;
+import accounts.domain.Transaction;
 import accounts.persistence.DataStore;
 import accounts.services.DatafileServices;
+import accounts.services.TransactionComparator;
 
 /**
  * Use the toOuput() routine to dump the demo database.
@@ -30,6 +35,22 @@ public class OprDynOutputDump
 			System.out.println();
 			PrintWriter out = new PrintWriter(System.out, true);
 			root.toOutput(out);
+
+			/*
+			 * Hack to show Transactions after Accounts
+			 */
+			System.out.println();
+			List tL = store.query(Transaction.class);
+
+			TreeSet sorted = new TreeSet(new TransactionComparator());
+			sorted.addAll(tL);
+
+			Iterator sI = sorted.iterator();
+			while (sI.hasNext()) {
+				Transaction t = (Transaction) sI.next();
+				t.toOutput(out);
+			}
+
 			out.flush();
 			System.out.println();
 			out.close();
