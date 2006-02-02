56e545da713f8667865052b0f4100d51ea981a15
Andrew Cowie <andrew@operationaldynamics.com>
2006-02-02 04:22:30 +0000

    Update ObjectiveAccounts main file to include copyright; add JavaDoc
    Add a public static field to the "global" ObjectiveAccounts class to
    document the copyright for this code (such that it ends up in the
    compiled bytecode and is quickly usable from about dialogs and
    whatnot) and a reference to the terms of use in the top level licence
    file.
    
    Add JavaDoc for the public static variables in this class.
    
    Fix LICENCE file to include 2006 in copyright statement.

--- src/accounts/client/ObjectiveAccounts.java
+++ src/accounts/client/ObjectiveAccounts.java
@@ -9,17 +9,50 @@ package accounts.client;
 import accounts.persistence.DataStore;
 
 /**
- * 
+ * The main reentry point for the whole framework. Holds a number of "global"
+ * variables for use throughout the program. See the JavaDoc for the public
+ * static members of this class. In particlar, note
+ * <ul>
+ * <li><code>store</code>, a reference to the currently open DataStore object.
+ * <li><code>ui</code>, a way to get to the top level UserInterface object so you can cause
+ * program wide GUI effects.
+ * </ul>
  * 
  * @author Andrew Cowie
  */
 public class ObjectiveAccounts
 {
-	public final static String VERSION = "0.1.9";
-	public static DataStore store = null;
-	
-//	public static ObjectiveAccountsUserInterface	ui	= null;
-	
+	/**
+	 * A String constant expressing the current version of the overall released
+	 * codebase. Used by equivalence (the top level ./configure program) to find
+	 * out the version to use in the names of release and snapshot tarballs.
+	 * Small helper programs making use of this class (ie to access
+	 * {@link ObjectiveAccounts.store}) do not need to override this constant.
+	 */
+	public static final String		VERSION		= "0.1.9";
+
+	/**
+	 * The copyright statement for this code and a pointer to the top level
+	 * LICENCE file. Iterate over this array and print with newlines or Labels
+	 * if you will, but this is here to embed it in distributed bytecode.
+	 */
+	public static final String[]	COPYRIGHT	= new String[] {
+			"Copyright © 2005-2006 Operational Dynamics Consulting Pty Ltd, and others.",
+			"The LICENCE file included with these sources lists the terms under which",
+			"you may use and redistribute this code.",
+												};
+	/**
+	 * The database which the program is talking to. There is frequent need for
+	 * code all over the system to have a quick way to get to the data store.
+	 * Rather than using the context idiom, we just hold a public static
+	 * reference here. Programs using the ObjectiveAccounts codebase will want
+	 * to open a data file and put the resultant reference here as a number of
+	 * classes (especaially Commands and Finders) rely on this being set.
+	 */
+	public static DataStore			store		= null;
+
+	// public static ObjectiveAccountsUserInterface ui = null;
+
 	/**
 	 * Go down hard. TODO Do we want to System.exit() here or elsewhere?
 	 * 
@@ -28,9 +61,9 @@ public class ObjectiveAccounts
 	 */
 	public static void abort(String message) {
 		System.err.println(message);
-//		if (ui != null) {
-//			ui.shutdown();
-//		}
+		// if (ui != null) {
+		// ui.shutdown();
+		// }
 	}
 
 }
