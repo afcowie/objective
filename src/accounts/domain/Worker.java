d2f744e28fb99cff922e82daf7a65b50ce863ccc
Andrew Cowie <andrew@operationaldynamics.com>
2006-03-20 13:39:33 +0000

    Add name field to Worker.

--- src/accounts/domain/Worker.java
+++ src/accounts/domain/Worker.java
@@ -2,14 +2,33 @@
  * Worker.java
  * 
  * See LICENCE file for usage and redistribution terms
- * Copyright (c) 2005 Operational Dynamics
+ * Copyright (c) 2005-2006 Operational Dynamics
  */
 package accounts.domain;
 
 /**
- * Base class of employees and subcontractors.
+ * Base class of employees and subcontractors. A Worker is someone we pay money
+ * to - periodically or otherwise.
  */
 public class Worker
 {
+	private String	name;
 
+	/**
+	 * Get the [full] name of the person.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Set the name of the Worker. This is full name; no need to bother with
+	 * first name/last name distinctions.
+	 * 
+	 * @param name
+	 *            a String with the full name of the person
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 }
