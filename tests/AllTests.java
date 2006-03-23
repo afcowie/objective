d89b47917fd49ee26194d0fa25bc1024db57db16
Andrew Cowie <andrew@operationaldynamics.com>
2006-03-23 06:31:29 +0000

    Consolidate test suite aggregation into single file
    We initially had a unit test aggregator per package, for example
    accounts.domain.AllDomainTests containing all the tests for
    accounts.domain, but with conceptual groups spreading across mulitple
    packages (ie accounts.services and country.au.services) this leads to
    either proliferation of these files or bluring seperation.
    
    So, directly add the unit test suites in the top level AllTests (with
    Eclipse nicely handling the imports) and remove the individual files
    All{Persistence,Domain,Services,Client}Tests.java in
    accounts.{persistence,domain,services,client} respectively.

--- tests/AllTests.java
+++ tests/AllTests.java
@@ -8,10 +8,27 @@
 import generic.junit.VerboseTestRunner;
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import accounts.client.AllClientTests;
-import accounts.domain.AllDomainTests;
-import accounts.persistence.AllPersistenceTests;
-import accounts.services.AllServicesTests;
+import accounts.client.Db4oActivationTest;
+import accounts.domain.BasicAccountTest;
+import accounts.domain.BasicLedgerTest;
+import accounts.domain.BasicLoanTest;
+import accounts.domain.BasicTransactionTest;
+import accounts.domain.DatestampTest;
+import accounts.domain.ForeignCurrencyTest;
+import accounts.domain.GenericTransactionTest;
+import accounts.domain.IdentifierTest;
+import accounts.persistence.BasicDb4oTest;
+import accounts.persistence.RollbackDb4oTest;
+import accounts.persistence.UnitOfWorkTest;
+import accounts.services.BasicCommandTest;
+import accounts.services.CommandsVsUnitsOfWorkTest;
+import accounts.services.ComparatorsDetailedTest;
+import accounts.services.ComparatorsInTreeSetTest;
+import accounts.services.DatafileTest;
+import accounts.services.EntityCommandTest;
+import accounts.services.IdentifierCommandsTest;
+import accounts.services.PostTransactionCommandTest;
+import country.au.services.AustralianPayrollTaxCalculatorTest;
 
 /**
  * @author Andrew Cowie
@@ -25,10 +42,34 @@ public class AllTests
 	public static Test suite() {
 		TestSuite suite = new TestSuite("All Unit Tests for ObjectiveAccounts");
 
-		suite.addTest(AllPersistenceTests.suite());
-		suite.addTest(AllDomainTests.suite());
-		suite.addTest(AllServicesTests.suite());
-		suite.addTest(AllClientTests.suite());
+		// persistence
+		suite.addTestSuite(BasicDb4oTest.class);
+		suite.addTestSuite(RollbackDb4oTest.class);
+		suite.addTestSuite(UnitOfWorkTest.class);
+
+		// domain
+		suite.addTestSuite(BasicAccountTest.class);
+		suite.addTestSuite(BasicLedgerTest.class);
+		suite.addTestSuite(BasicTransactionTest.class);
+		suite.addTestSuite(GenericTransactionTest.class);
+		suite.addTestSuite(DatestampTest.class);
+		suite.addTestSuite(BasicLoanTest.class);
+		suite.addTestSuite(ForeignCurrencyTest.class);
+		suite.addTestSuite(IdentifierTest.class);
+
+		// services
+		suite.addTestSuite(DatafileTest.class);
+		suite.addTestSuite(BasicCommandTest.class);
+		suite.addTestSuite(CommandsVsUnitsOfWorkTest.class);
+		suite.addTestSuite(PostTransactionCommandTest.class);
+		suite.addTestSuite(ComparatorsDetailedTest.class);
+		suite.addTestSuite(ComparatorsInTreeSetTest.class);
+		suite.addTestSuite(EntityCommandTest.class);
+		suite.addTestSuite(IdentifierCommandsTest.class);
+		suite.addTestSuite(AustralianPayrollTaxCalculatorTest.class);
+
+		// client
+		suite.addTestSuite(Db4oActivationTest.class);
 
 		return suite;
 	}
