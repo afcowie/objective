dd2429b91100f6f162df21c5d83a088353ef997b
Andrew Cowie <andrew@operationaldynamics.com>
2006-05-02 05:38:38 +0000

    Add ReimbursableExpensesTransaction

---
+++ src/accounts/domain/ReimbursableExpensesTransaction.java
@@ -0,0 +1,45 @@
+/*
+ * ReimbursableExpensesTransaction.java
+ * 
+ * See LICENCE file for usage and redistribution terms
+ * Copyright (c) 2006 Operational Dynamics
+ */
+package accounts.domain;
+
+/**
+ * Expenses reimbursable to an Worker
+ * 
+ * @author Andrew Cowie
+ */
+public class ReimbursableExpensesTransaction extends Transaction
+{
+	/**
+	 * The person who the expenses are reimbursable to.
+	 */
+	private Worker	worker;
+
+	// FUTURE maybe a "main" Entry? Common case; if we need it, push it up to a
+	// superclass.
+
+	public ReimbursableExpensesTransaction() {
+		super();
+	}
+
+	public Worker getWorker() {
+		return worker;
+	}
+
+	/**
+	 * Set the person to whom the expeneses must be reimbursed.
+	 */
+	public void setWorker(Worker worker) {
+		if (worker == null) {
+			throw new IllegalArgumentException("Can't set null as the Worker you're reimbursing");
+		}
+		this.worker = worker;
+	}
+
+	public String getClassString() {
+		return "Reimbursable Expenses";
+	}
+}
