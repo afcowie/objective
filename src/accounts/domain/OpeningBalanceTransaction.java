6812b5f1d2abef7a1d7b87b3fd75dad9143522a0
Andrew Cowie <andrew@operationaldynamics.com>
2011-02-16 12:32:03 +1100

    Apply proper GPL headers

--- src/accounts/domain/OpeningBalanceTransaction.java
+++ src/accounts/domain/OpeningBalanceTransaction.java
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GPL along with this program. If not,
  * see http://www.gnu.org/licenses/. The authors of this program may be
- * contacted through http://research.operationaldynamics.com/projects/objective/.
+ * contacted via http://research.operationaldynamics.com/projects/objective/.
  */
 package accounts.domain;
 
