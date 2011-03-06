44e42ac83b9d877240063fc9571875cea8c68408
Andrew Cowie <andrew@operationaldynamics.com>
2011-03-07 09:04:04 +1100

    Fix lack of generics in AccountComparator

--- src/accounts/services/AccountComparator.java
+++ src/accounts/services/AccountComparator.java
@@ -23,27 +23,27 @@ import generic.util.DebugException;
 import java.util.Comparator;
 import java.util.TreeMap;
 
-import accounts.domain.Account;
-import accounts.domain.AccountsPayable;
-import accounts.domain.AccountsReceivable;
-import accounts.domain.AssetAccount;
-import accounts.domain.BankAccount;
-import accounts.domain.CashAccount;
-import accounts.domain.CreditPositiveAccount;
+import objective.domain.Account;
+import objective.domain.AccountsPayableAccount;
+import objective.domain.AccountsReceivableAccount;
+import objective.domain.AssetAccount;
+import objective.domain.BankAccount;
+import objective.domain.CashAccount;
+import objective.domain.CreditPositiveAccount;
+import objective.domain.DebitPositiveAccount;
+import objective.domain.DepreciatingAssetAccount;
+import objective.domain.EquityAccount;
+import objective.domain.ExpenseAccount;
+import objective.domain.GenericExpenseAccount;
+import objective.domain.LiabilityAccount;
+import objective.domain.OwnersEquityAccount;
+import objective.domain.ProfessionalRevenueAccount;
+import objective.domain.ReimbursableExpensesPayableAccount;
+import objective.domain.SalesTaxPayableAccount;
 import accounts.domain.CurrencyGainLossAccount;
-import accounts.domain.DebitPositiveAccount;
-import accounts.domain.DepreciatingAssetAccount;
-import accounts.domain.EquityAccount;
-import accounts.domain.ExpenseAccount;
-import accounts.domain.GenericExpenseAccount;
-import accounts.domain.LiabilityAccount;
 import accounts.domain.LoanPayableAccount;
-import accounts.domain.OwnersEquityAccount;
 import accounts.domain.PayrollTaxPayableAccount;
-import accounts.domain.ProfessionalRevenueAccount;
 import accounts.domain.RevenueAccount;
-import accounts.domain.SalesTaxPayableAccount;
-import accounts.domain.ReimbursableExpensesPayableAccount;
 
 /**
  * Sort Accounts to match our version of accounting order. Accounts, as
@@ -58,12 +58,12 @@ import accounts.domain.ReimbursableExpensesPayableAccount;
 /*
  * Move to Account? No, because it references so many deep classes.
  */
-public class AccountComparator implements Comparator
+public class AccountComparator implements Comparator<Account>
 {
-    private final static TreeMap lookupTable;
+    private final static TreeMap<String, Integer> lookupTable;
 
     static {
-        final Class[] ordering = {
+        final Class<?>[] ordering = {
             Account.class,
             DebitPositiveAccount.class,
             CreditPositiveAccount.class,
@@ -71,11 +71,11 @@ public class AccountComparator implements Comparator
             AssetAccount.class,
             BankAccount.class,
             CashAccount.class,
-            AccountsReceivable.class,
+            AccountsReceivableAccount.class,
             DepreciatingAssetAccount.class,
 
             LiabilityAccount.class,
-            AccountsPayable.class,
+            AccountsPayableAccount.class,
             SalesTaxPayableAccount.class,
             PayrollTaxPayableAccount.class,
             ReimbursableExpensesPayableAccount.class,
@@ -92,7 +92,7 @@ public class AccountComparator implements Comparator
             GenericExpenseAccount.class,
         };
 
-        lookupTable = new TreeMap();
+        lookupTable = new TreeMap<String, Integer>();
 
         for (int i = 0; i < ordering.length; i++) {
             lookupTable.put(ordering[i].getName(), new Integer(i));
@@ -102,7 +102,7 @@ public class AccountComparator implements Comparator
     private final int indexOf(Object obj) {
         final String className = obj.getClass().getName();
 
-        final Integer index = (Integer) lookupTable.get(className);
+        final Integer index = lookupTable.get(className);
 
         if (index == null) {
             throw new DebugException("You are attempting to look up a " + obj.getClass().getName()
@@ -113,25 +113,18 @@ public class AccountComparator implements Comparator
         return index.intValue();
     }
 
-    public int compare(Object o1, Object o2) {
-        /*
-         * This thing only orders Accounts!
-         */
-        if ((!(o1 instanceof Account)) || (!(o2 instanceof Account))) {
-            throw new ClassCastException("This comparator only orders Account objects");
-        }
+    public int compare(final Account a1, final Account a2) {
+        final int i1, i2, hash1, hash2;
+        final String code1, code2, title1, title2;
 
-        int i1 = indexOf(o1);
-        int i2 = indexOf(o2);
+        i1 = indexOf(a1);
+        i2 = indexOf(a2);
 
         if (i1 < i2) {
             return -1;
         } else if (i1 > i2) {
             return +1;
         } else {
-            Account a1 = (Account) o1;
-            Account a2 = (Account) o2;
-
             /*
              * Same Account subclass, so look into Account's code and order by
              * it. Note that we generally aren't using this at the moment, so
@@ -139,8 +132,8 @@ public class AccountComparator implements Comparator
              * alternate ordering. TODO perhaps this should rise above the
              * result of the indexOf call?
              */
-            String code1 = a1.getCode();
-            String code2 = a2.getCode();
+            code1 = a1.getCode();
+            code2 = a2.getCode();
 
             int codeCmp;
             if ((code1 == null) || (code2 == null)) {
@@ -158,8 +151,8 @@ public class AccountComparator implements Comparator
                  * Same (or as expected and more likely, null) code, so look
                  * into Account's name and order by that.
                  */
-                String title1 = a1.getTitle();
-                String title2 = a2.getTitle();
+                title1 = a1.getTitle();
+                title2 = a2.getTitle();
 
                 int titleCmp;
                 if ((title1 == null) || (title2 == null)) {
@@ -177,8 +170,8 @@ public class AccountComparator implements Comparator
                      * At this point, go to Object's hashCode. Do not expect
                      * to get this deep in normal usage.
                      */
-                    int hash1 = a1.hashCode();
-                    int hash2 = a2.hashCode();
+                    hash1 = a1.hashCode();
+                    hash2 = a2.hashCode();
 
                     if (hash1 < hash2) {
                         return -1;
